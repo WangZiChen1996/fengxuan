b9a0da72f35575763c753a2e1f65a14e2fc20fb5
wzc <991802672@qq.com>
2020-02-12 23:00:29 +0800

    Add simple code

--- js/baidu-tongji.js
+++ js/baidu-tongji.js
@@ -2,8 +2,9 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?e1810c8f323c4c1edc94d09ac58fdb9c";
+  hm.src = "https://hm.baidu.com/hm.js?c103c7d79f77bbfa2a62d6f573329e1e";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
 
