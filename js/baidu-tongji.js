71df5f7ece90a7a075a490cf8c69b4182aea5891
wzc <991802672@qq.com>
2020-02-12 22:48:14 +0800

    Add simple code

---
+++ js/baidu-tongji.js
@@ -0,0 +1,9 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?e1810c8f323c4c1edc94d09ac58fdb9c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
